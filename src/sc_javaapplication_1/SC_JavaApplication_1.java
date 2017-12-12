408e8e1af80325e28d2ce0beeaf24f5967b12083
Simone Cianni <simone.cianni@bticino.it>
2017-12-12 16:10:40 +0100

    Create empty java application with netbeans
    Signed-off-by: Simone Cianni <simone.cianni@bticino.it>

---
+++ src/sc_javaapplication_1/SC_JavaApplication_1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sc_javaapplication_1;
+
+/**
+ *
+ * @author simone
+ */
+public class SC_JavaApplication_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("My First JProgam");
+    }
+    
+}
