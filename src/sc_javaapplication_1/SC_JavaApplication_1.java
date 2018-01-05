56182dc9d2df08bb17a9f5185be5ea49f4b64bd7
Simone Cianni <simone.cianni@bticino.it>
2018-01-05 11:25:15 +0100

    Add code for logging
    Signed-off-by: Simone Cianni <simone.cianni@bticino.it>

--- src/sc_javaapplication_1/SC_JavaApplication_1.java
+++ src/sc_javaapplication_1/SC_JavaApplication_1.java
@@ -11,12 +11,46 @@ package sc_javaapplication_1;
  */
 public class SC_JavaApplication_1 {
 
+    String name;
+    int age;
+
+    public static void log(String message) {
+       String fullClassName =
+               Thread.currentThread().getStackTrace()[2].getClassName();
+       String className =
+               fullClassName.substring(fullClassName.lastIndexOf(".") + 1);
+       String methodName =
+               Thread.currentThread().getStackTrace()[2].getMethodName();
+       int lineNumber =
+               Thread.currentThread().getStackTrace()[2].getLineNumber();
+
+       System.out.println(className + "." + methodName
+                        + "(" + lineNumber + "): "
+                        + message);
+    }
+    static float getDisount(int price)
+    {
+        float discount;
+        //log(" price"+price);
+        discount = price*20/100;
+        //log(" discount"+discount);
+        return discount;
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("My First JProgam");
+        int number = 100;
+        float discount = getDisount(number);
+        log("My First JProgam");
+        log("Applico sconto " + number +" - " +getDisount(number)+" -> "
+                +(number-discount));
+    //    SC_JavaApp.name = "Simone";
+    //    age = 40;
+    //    log(+name+" anni "+age);
     }
-    
+
+
 }
